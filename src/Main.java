a294c92b7115595aae4a14a39013fa0dad7c357e
Kuba Milcarz <kubamilcarz@yahoo.com>
2024-10-07 18:45:54 +0200

    Changed parameters of add

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@
 public class Main {
     public static void main(String[] args) {
         Adder adder = new Adder();
-        System.out.println(adder.add(1, 2));
+        System.out.println(adder.add(6, 3));
 
         Subtractor subtractor = new Subtractor();
         System.out.println(subtractor.subtract(6, 3));
