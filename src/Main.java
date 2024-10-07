488c9dc5ebab2aa8dec8b04365148674b07d1eb5
Kuba Milcarz <kubamilcarz@yahoo.com>
2024-10-07 17:47:55 +0200

    first push

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Adder adder = new Adder();
+        System.out.println(adder.add(1, 2));
+
+        Subtractor subtractor = new Subtractor();
+        System.out.printl(subtractor.subtract(6, 3));
+    }
+}
