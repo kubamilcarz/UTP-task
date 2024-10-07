84ffcd8839c7db682103cd53d326dc62813c6cac
Kuba Milcarz <kubamilcarz@yahoo.com>
2024-10-07 17:51:35 +0200

    Added your current task

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+// TODO: we need to add the missing classes!
+
 public class Main {
     public static void main(String[] args) {
         Adder adder = new Adder();
